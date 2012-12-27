8e2401a11e36b06d498627acc663c58548e67dfd
Taha Azab <azab.taha@gmail.com>
2012-12-28 01:45:10 +0200

    modified app.config & added logging configurations

--- LoggingApplicationBlockDemo/Program.cs
+++ LoggingApplicationBlockDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
 
 namespace LoggingApplicationBlockDemo
 {
@@ -10,6 +11,7 @@ namespace LoggingApplicationBlockDemo
     {
         static void Main(string[] args)
         {
+            var container = new UnityContainer();
         }
     }
 }
