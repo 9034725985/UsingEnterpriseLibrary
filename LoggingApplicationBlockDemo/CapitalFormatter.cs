f6652a207aaba44c58b04ed1bce91bd9ebec1e01
Taha Azab <azab.taha@gmail.com>
2012-12-28 14:40:20 +0200

    added CapitalFormatter as custom logging formatter

---
+++ LoggingApplicationBlockDemo/CapitalFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Formatters;
+
+namespace LoggingApplicationBlockDemo
+{
+    [ConfigurationElementType(typeof(CustomFormatterData))]
+    public class CapitalFormatter:ILogFormatter
+    {
+        public CapitalFormatter(NameValueCollection collection)
+        {
+            
+        }
+        public string Format(LogEntry log)
+        {
+            return log.Message.ToUpper();
+        }
+    }
+}
