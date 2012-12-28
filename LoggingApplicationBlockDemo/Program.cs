dd45faea8377836d3ebb283020bd31ea25cc91bb
Taha Azab <azab.taha@gmail.com>
2012-12-28 10:11:27 +0200

    added filters to app.confige

--- LoggingApplicationBlockDemo/Program.cs
+++ LoggingApplicationBlockDemo/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Unity;
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Unity;
 using Microsoft.Practices.EnterpriseLibrary.Logging;
 using Microsoft.Practices.Unity;
 
