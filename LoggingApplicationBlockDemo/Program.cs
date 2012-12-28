ea5b05bd36506196ebc757589eff522fb1d7be80
Taha Azab <azab.taha@gmail.com>
2012-12-28 09:21:18 +0200

    logging with LogEntry class object

--- LoggingApplicationBlockDemo/Program.cs
+++ LoggingApplicationBlockDemo/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Unity;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
 using Microsoft.Practices.Unity;
 
 namespace LoggingApplicationBlockDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var container = new UnityContainer();
+            container.AddNewExtension<EnterpriseLibraryCoreExtension>();
+
+            var logWriter = container.Resolve<LogWriter>();
+            //logWriter.Write("I am Logging!");
+            //logWriter.Write("I am Logging!", "Debug", 1, 3000, TraceEventType.Error, "Error Message");
+
+
+            var logEntry = new LogEntry
+                {
+                    Message = "Logging with LogEntry",
+                    Categories = new List<string> {"Debug", "UI"},
+                    Priority = 3,
+                    EventId = 3000,
+                    Title = "Log Title",
+                    Severity = TraceEventType.Warning
+                };
+
+            logWriter.Write(logEntry);
+
         }
     }
 }
