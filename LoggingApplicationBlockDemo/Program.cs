6765c23e5adf41539feb51e0d82e130253f1afe1
Taha Azab <azab.taha@gmail.com>
2012-12-28 11:30:57 +0200

    added Extended Properties

--- LoggingApplicationBlockDemo/Program.cs
+++ LoggingApplicationBlockDemo/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Unity;
+﻿using System.Collections.Generic;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Unity;
 using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging.ExtraInformation;
 using Microsoft.Practices.Unity;
 
 namespace LoggingApplicationBlockDemo
@@ -31,11 +33,35 @@ namespace LoggingApplicationBlockDemo
             //if (logWriter.IsLoggingEnabled())
             //    logWriter.Write("Logging using filter", "Demo");
 
-            logWriter.Write("Demo Logging Message", "Demo");
-            logWriter.Write("UI Logging Message", "UI");
-            logWriter.Write("Another UI Logging Message", "UI");
-            logWriter.Write("Data Logging Messager", "Data");
+            //logWriter.Write("Demo Logging Message", "Demo");
+            //logWriter.Write("UI Logging Message", "UI");
+            //logWriter.Write("Another UI Logging Message", "UI");
+            //logWriter.Write("Data Logging Messager", "Data");
 
+            var logEntry = new LogEntry
+                {
+                    Message = "Detailed Message", Categories = new List<string> {"General"}
+                };
+
+            var extendedInfo = new Dictionary<string, object>();
+
+            var debugInfoProvider = new DebugInformationProvider();
+            debugInfoProvider.PopulateDictionary(extendedInfo);
+
+            var managedSecurity = new ManagedSecurityContextInformationProvider();
+            managedSecurity.PopulateDictionary(extendedInfo);
+
+            var unmanagedSecurity = new UnmanagedSecurityContextInformationProvider();
+            unmanagedSecurity.PopulateDictionary(extendedInfo);
+
+            var comInfo = new ComPlusInformationProvider();
+            comInfo.PopulateDictionary(extendedInfo);
+
+            extendedInfo.Add("My Custom Info", ".... INFO ....");
+
+            logEntry.ExtendedProperties = extendedInfo;
+
+            logWriter.Write(logEntry);
         }
     }
 }
