67435be370d14fe5f5b236d80bea3908b2ccbca7
Taha Azab <azab.taha@gmail.com>
2012-12-29 23:17:23 +0200

    using custom refersh action

---
+++ CachingBlockDemo/FileRefreshAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+using Microsoft.Practices.EnterpriseLibrary.Caching.Expirations;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+namespace CachingBlockDemo
+{
+    [Serializable]
+    public class FileRefreshAction : ICacheItemRefreshAction
+    {
+        private readonly string _filename;
+
+        public FileRefreshAction(string filename)
+        {
+            _filename = filename;
+        }
+
+        public void Refresh(string removedKey, object expiredValue, CacheItemRemovedReason removalReason)
+        {
+            var cacheManager = EnterpriseLibraryContainer.Current.GetInstance<ICacheManager>();
+            var fileContents = File.ReadAllText(_filename);
+            var refreshAction = new FileRefreshAction(_filename);
+            var expiration = new FileDependency(_filename);
+
+            cacheManager.Add(removedKey, fileContents, CacheItemPriority.Normal, refreshAction, expiration);
+        }
+    }
+}
